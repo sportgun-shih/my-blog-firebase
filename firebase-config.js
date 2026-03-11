5c1a0588a778767c6824c03f37b1d5d4361f950b
sportgun-shih <sportgun@gmail.com>
2026-03-11 20:45:03 +0800

    將檔案移動到 web 資料夾

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@
  */
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDK5u8B-0ehVHix9Whc-b7rM-Q8LmcF5Lo",
-    authDomain: "hai-teaching.firebaseapp.com",
-    projectId: "hai-teaching",
-    storageBucket: "hai-teaching.firebasestorage.app",
-    messagingSenderId: "700712390542",
-    appId: "1:700712390542:web:0118ccc9cd9497d5121edf",
-    measurementId: "G-QKCYGQBKNB"
+  apiKey: "AIzaSyDK5u8B-0ehVHix9Whc-b7rM-Q8LmcF5Lo",
+  authDomain: "hai-teaching.firebaseapp.com",
+  projectId: "hai-teaching",
+  storageBucket: "hai-teaching.firebasestorage.app",
+  messagingSenderId: "700712390542",
+  appId: "1:700712390542:web:0118ccc9cd9497d5121edf",
+  measurementId: "G-QKCYGQBKNB"
 };
 
 // 初始化 Firebase
